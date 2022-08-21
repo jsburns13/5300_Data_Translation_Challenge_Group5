def145aa1b21592a47ad9a9307a241439d556fcc
jsburns13 <86275366+jsburns13@users.noreply.github.com>
2022-08-21 15:23:23 -0700

    Initial commit for Burns data work

--- code/Burns_Data_Wrangling.R
+++ code/Burns_Data_Wrangling.R
@@ -1,5 +1,26 @@
 library(tidyverse)
+library(ipumsr)
 
-filename = "fake_data.csv"
+ddi <- read_ipums_ddi("data/cps_00004.xml")
+data <- read_ipums_micro(ddi)
 
-read_csv("data/fake_data.csv")
+data <- data %>%
+  filter(YEAR>=2019 & YEAR <= 2021) %>%
+  mutate(Employment = case_when(
+    EMPSTAT == 10 | EMPSTAT == 12 ~ 1,
+    TRUE ~ 0
+  )) %>%
+  mutate(date = ISOdate(YEAR,MONTH,1)) %>%
+  mutate(IND1950 = as.factor(IND1950)) %>%
+  # https://cps.ipums.org/cps/codes/ind_2014_codes.shtml
+  mutate(Retail = as.factor(case_when(
+    IND1950 == "1190" | IND1950 == "5470" | IND1950 == "5580" | IND1950 == "5790" ~ 1,
+    TRUE ~ 0
+  )))
+
+mean_data <- data %>%
+  group_by(Retail, date) %>%
+  summarise(avg_employment = mean(Employment))
+
+ggplot(data = mean_data, aes(x=date, y=avg_employment, colour=Retail, group=Retail)) +
+  geom_point() + geom_line()
